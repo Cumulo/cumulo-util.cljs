d8f5ae11215e6a268f2502f60356fa83235ff97a
jiyinyiyong <jiyinyiyong@gmail.com>
2018-12-15 01:57:02 +0800

    add delay! id! repeat! unix-time! ; bump 0.1.0-a3

--- src/cumulo_util/core.cljs
+++ src/cumulo_util/core.cljs
@@ -1,6 +1,14 @@
 
-(ns cumulo-util.core )
+(ns cumulo-util.core (:require ["shortid" :as shortid]))
+
+(defn delay! [duration task] (js/setTimeout task duration))
 
 (defn find-first [f xs] (reduce (fn [_ x] (when (f x) (reduced x))) nil xs))
 
 (defn get-env! [property] (aget (.-env js/process) property))
+
+(defn id! [] (.generate shortid))
+
+(defn repeat! [duration task] (js/setInterval task duration))
+
+(defn unix-time! [] (.valueOf (js/Date.)))
